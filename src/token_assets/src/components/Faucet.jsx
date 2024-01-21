8d5fb7aa9e1868ebf43fecf0b1e48a8f485648d9
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-01-21 00:38:00 +0000

    complete authentication with internet identity

--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 import { token } from "../../../declarations/token";
+import { AuthClient } from "../../../../node_modules/@dfinity/auth-client/lib/cjs/index";
+import { canisterId, createActor } from "../../../declarations/token/index";
 
 function Faucet() {
   const [disable, setDisable] = useState(false);
   const [buttonText, setButtonText] = useState("Gimme gimme");
-
+  
   async function handleClick(event) {
     setDisable(true);
+
+    /* Use this when deploying to Internet Computer
+
+    const authClient = await AuthClient.create();
+    const indentity = await authClient.getIdentity();
+
+    const authenticatedCanister = createActor(canisterId, {
+      agentOptions: {
+        indentity,
+      },
+    });
+
+    const results = await authenticatedCanister.payOut();
+
+    -- remove this below and use the one above: const results = await token.payOut(); ---
+    */
+    
     const results = await token.payOut();
     setButtonText(results);
   }
-
   return (
     <div className="blue window">
       <h2>
@@ -20,8 +38,7 @@ function Faucet() {
         Faucet
       </h2>
       <label>
-        Get your free TOK tokens here! Claim 10,000 TOK tokens to your
-        account.
+        Get your free TOK tokens here! Claim 10,000 TOK tokens to your account.
       </label>
       <p className="trade-buttons">
         <button id="btn-payout" onClick={handleClick} disabled={disable}>
--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,11 +1,26 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
+import ReactDOM from "react-dom";
+import React from "react";
 import App from "./components/App";
+import { AuthClient } from "@dfinity/auth-client";
 
-const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
-}
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (authClient.isAuthenticated()) {
+    console.log("Logged in!!");
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      indentityProvider: "https://indentity.ic0.app/#authorize",
+      onSuccess: () => {
+        handleAuthenticated(authClient);
+      },
+    });
+  }
+
+  async function handleAuthenticated(authClient) {
+    ReactDOM.render(<App />, document.getElementById("root"));
+  }
+};
 
 init();
-
-
